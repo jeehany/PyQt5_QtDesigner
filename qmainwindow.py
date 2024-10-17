54dafc4b575b167fe44105a506a590c7194807db
jeehany <sriemilyani@gmail.com>
2024-10-17 22:38:24 +0800

    first commit

---
+++ qmainwindow.py
@@ -0,0 +1,15 @@
+
+from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QMainWindow
+
+app = QApplication([])
+
+window = QMainWindow()
+label = QLabel(window)
+label.setText("Label1")
+label.move(200, 0)
+
+button = QPushButton(window)
+button.setText("Button")
+
+window.show()
+app.exec_()
